a72f239c5c7e1899fddc8a0580edf8c41b6eb38b
Ugeshraja <ugeshraja007@gmail.com>
2026-07-08 16:49:14 +0530

    Fix API URL 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "https://exampro-online-examination-system.onrender.com/api";
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://exampro-online-examination-system.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+export default api;
 
 export default api;
 
