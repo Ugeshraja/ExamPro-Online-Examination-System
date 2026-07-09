dc637aa49b5c47b5e2b21dfae0e1408049a6ce1a
Ugeshraja <ugeshraja007@gmail.com>
2026-07-09 09:39:34 +0530

    Escape reserved MySQL keyword rank in Leaderboard entity

--- backend/src/main/java/com/exampro/entity/Leaderboard.java
+++ backend/src/main/java/com/exampro/entity/Leaderboard.java
@@ -27,7 +27,7 @@ public class Leaderboard {
     @Column(nullable = false)
     private Double percentage;
 
-    @Column(name = "rank", nullable = false)
+    @Column(name = "`rank`", nullable = false)
     private Integer rank;
 
     @Column(name = "updated_at")
